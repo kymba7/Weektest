7e4fdacbe3553e67917bf3d2d8f4cc35eebc5c0a
Kymbrlee <kymba7@gmail.com>
2018-01-12 15:22:12 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! Please enter your name.");
+        String name = sc.nextLine();
+        System.out.println("Enter your age.");
+        String age = sc.nextLine();
+        System.out.println("Enter the current year.");
+        String year = sc.nextLine();
+
+        String [] questions = {
+                "Hello! Please enter your name.",
+                "Enter your current age.",
+                "Enter the current year.",
+
+        };
+        int age = Integer.parseInt(ageString)
+        int curAge = age +1;
+        int CurAge = age + 5;
+        int CurAge = age + 10;
+
+        int year = Integer.parseInt(yrString);
+        int CurY = year + 1;
+        int CurY = year + 5;
+        int CurY = year + 10;
+
+
+                i
+
+
+
+
+
+    }
+}
